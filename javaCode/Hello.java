9b90cd5ecdf352cb9b3c67d15ddb934ab78962f6
chenxin <2567853730@qq.com>
2024-08-28 01:53:39 +0800

    第一次提交

---
+++ javaCode/Hello.java
@@ -0,0 +1,9 @@
+//java快速入门
+//表示有一个交Hello的共有类
+public class Hello{
+
+	//编写一个主方法
+	public static void main(String[] args) {
+		System.out.println("Hello,world");
+	}
+}
---
+++ javaCode/work001.java
@@ -0,0 +1,5 @@
+public class work001{
+	public static void main(String[] args){
+		System.out.println("³ÂöÎ is studying java");
+	}
+}
